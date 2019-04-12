b32df77b17d2ca3942b1841abedb4cb414966c17
Volodymyr Byelobrov <49191009+sh-am-si@users.noreply.github.com>
2019-04-12 13:16:40 +0200

    Add files via upload

---
+++ pascals_triangle.py
@@ -0,0 +1,17 @@
+'''
+Introduction to python
+Lecture 2: Sequences and Comprehensions
+
+Pascal's triangle
+'''
+
+triangle = [[1]]
+
+num = 100
+
+for i in range(num):
+    last = triangle[-1][:]
+    triangle.append([1 if i == 0 else last[i - 1] + last[i] for i in range(len(last))] + [1])
+
+for i in range(len(triangle)):
+    print(i, ':', triangle[i])
