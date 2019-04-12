beeacf1508224dd0d6593b806c4860a6872bfd15
Volodymyr Byelobrov <49191009+sh-am-si@users.noreply.github.com>
2019-04-12 13:17:47 +0200

    Update pascals_triangle.py

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -10,7 +10,7 @@ triangle = [[1]]
 num = 100
 
 for i in range(num):
-    last = triangle[-1][:]
+    last = triangle[-1]
     triangle.append([1 if i == 0 else last[i - 1] + last[i] for i in range(len(last))] + [1])
 
 for i in range(len(triangle)):
